829047224885b75f7b1459d678117191ea125166
Brendan Drobek <Kleinbottled@Brendans-MBP.sd.cox.net>
2015-04-21 08:17:31 -0700

    Install gulp. Refactor main.js for better framerate

--- views/js/main.js
+++ views/js/main.js
@@ -503,8 +503,9 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var scrollPos = (document.body.scrollTop / 1250); //Defining this variable outside the for loop prevents forced sycronous layout
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin(scrollPos + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
@@ -525,7 +526,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+  for (var i = 0; i < 40; i++) { //Lowered pizza count (most are still offscreen) to improve performance
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
